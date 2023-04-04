29ac10957284ebb4e0fa8efdf88aaa58e50f17cf
Roger Viñas <roger.vinas@n26.com>
2023-04-04 20:58:03 +0200

    Wait for vault-cli

--- springboot-app/src/test/kotlin/org/rogervinas/GreetingApplicationTest.kt
+++ springboot-app/src/test/kotlin/org/rogervinas/GreetingApplicationTest.kt
@@ -24,6 +24,7 @@ class GreetingApplicationTest {
           .withLocalCompose(true)
           .waitingFor("db", forLogMessage(".*database system is ready to accept connections.*", 1))
           .waitingFor("vault", forLogMessage(".*Development mode.*", 1))
+          .waitingFor("vault-cli", forLogMessage(".*created_time.*", 1))
   }
 
   @Autowired
