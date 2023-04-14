88980f809493a8bf3bf72b28d2d6c15eb4ae511f
Roger Viñas Alcon <roger.vinas@fastzink.com>
2023-04-14 15:38:03 +0200

    Http4k App Repo (#17)
    * Add repo
    
    * Remove ping

---
+++ http4k-app/src/main/kotlin/org/rogervinas/GreetingController.kt
@@ -0,0 +1,18 @@
+package org.rogervinas
+
+import org.http4k.core.Method.GET
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+
+fun greetingController(
+  name: String,
+  secret: String,
+  repository: GreetingRepository
+) = routes(
+  "/hello" bind GET to {
+    Response(Status.OK)
+      .body("${repository.getGreeting()} my name is $name and my secret is $secret")
+  }
+)
