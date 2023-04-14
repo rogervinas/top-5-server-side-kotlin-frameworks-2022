1cdb00ba6b5b8e6040cda0e22266dc76e63b9a32
Roger Viñas Alcon <roger.vinas@fastzink.com>
2023-04-14 18:08:53 +0200

    Ktor app refactor (#20)

--- ktor-app/src/main/kotlin/org/rogervinas/GreetingApplication.kt
+++ ktor-app/src/main/kotlin/org/rogervinas/GreetingApplication.kt
@@ -3,9 +3,13 @@ package org.rogervinas
 import com.bettercloud.vault.Vault
 import com.bettercloud.vault.VaultConfig
 import io.ktor.server.application.Application
+import io.ktor.server.config.ApplicationConfig
+import io.ktor.server.config.MapApplicationConfig
+import io.ktor.server.config.mergeWith
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.EngineMain
 import io.ktor.server.netty.Netty
+import java.sql.DriverManager
 
 fun main(args: Array<String>) {
   EngineMain.main(args)
@@ -19,24 +23,35 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
-  val vaultData = vaultData()
-  val repository = greetingRepository()
+  val environmentConfig = environment.config.withVault()
+  val repository = greetingRepository(environmentConfig)
   greetingController(
-    environment.config.property("greeting.name").getString(),
-    vaultData["greeting.secret"] ?: "unknown",
+    environmentConfig.property("greeting.name").getString(),
+    environmentConfig.propertyOrNull("greeting.secret")?.getString() ?: "unknown",
     repository
   )
 }
 
-private fun Application.vaultData(): Map<String, String> {
-  val vaultProtocol = environment.config.property("vault.protocol").getString()
-  val vaultHost = environment.config.property("vault.host").getString()
-  val vaultPort = environment.config.property("vault.port").getString()
-  val vaultToken = environment.config.property("vault.token").getString()
-  val vaultPath = environment.config.property("vault.path").getString()
+private fun greetingRepository(config: ApplicationConfig): GreetingRepository {
+  val host = config.property("database.host").getString()
+  val port = config.property("database.port").getString()
+  val name = config.property("database.name").getString()
+  val username = config.property("database.username").getString()
+  val password = config.property("database.password").getString()
+  val connection = DriverManager.getConnection("jdbc:postgresql://$host:$port/$name", username, password)
+  return GreetingJdbcRepository(connection)
+}
+
+private fun ApplicationConfig.withVault(): ApplicationConfig {
+  val vaultProtocol = this.property("vault.protocol").getString()
+  val vaultHost = this.property("vault.host").getString()
+  val vaultPort = this.property("vault.port").getString()
+  val vaultToken = this.property("vault.token").getString()
+  val vaultPath = this.property("vault.path").getString()
   val vaultConfig = VaultConfig()
     .address("$vaultProtocol://$vaultHost:$vaultPort")
     .token(vaultToken)
     .build()
-  return Vault(vaultConfig).logical().read(vaultPath).data.toMap()
+  val vaultData = Vault(vaultConfig).logical().read(vaultPath).data
+  return this.mergeWith(MapApplicationConfig(vaultData.entries.map { Pair(it.key, it.value) }))
 }
