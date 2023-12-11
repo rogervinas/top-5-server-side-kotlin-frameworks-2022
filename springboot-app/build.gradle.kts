d8a7f6193995c1570f53b27b1c9bb6740477ccbe
Roger Viñas Alcon <roger.vinas@fastzink.com>
2023-12-11 11:41:02 +0100

    Update build.gradle.kts

--- springboot-app/build.gradle.kts
+++ springboot-app/build.gradle.kts
@@ -11,7 +11,6 @@ java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
   mavenCentral()
-  maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 val springCloudVersion = "2023.0.0"
