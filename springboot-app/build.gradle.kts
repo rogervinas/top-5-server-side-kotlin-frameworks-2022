ca9d266ffb789913ffec3fb4e2c5c70eb260f5b8
Roger Vinas <roger.vinas@fastzink.com>
2025-12-06 19:12:47 +0100

    Fix missing

--- springboot-app/build.gradle.kts
+++ springboot-app/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
   implementation("org.springframework.cloud:spring-cloud-starter-vault-config")
 
   implementation("org.springframework.boot:spring-boot-starter-jdbc")
+  implementation("org.springframework.boot:spring-boot-starter-flyway")
   implementation("org.flywaydb:flyway-core:$flywayVersion")
   implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
   implementation("org.postgresql:postgresql:42.7.8")
