7bdfec3c273d9feb1019fcbe8482041a33652771
Roger Vinas <roger.vinas@fastzink.com>
2025-12-06 19:01:10 +0100

    Fix missing

--- springboot-app/build.gradle.kts
+++ springboot-app/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-webclient-test")
 
   testImplementation("org.testcontainers:junit-jupiter:1.21.3")
   testImplementation("org.assertj:assertj-core:3.27.6")
